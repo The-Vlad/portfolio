7ec16c0fd6e4456838d6fa5803811a5230d5ff32
Эмиль <akhtyamkinemil2004@gmail.com>
2022-09-10 20:02:45 +0400

    Initial commit

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request, 'main/about.html')
+
+def index(request):
+    return render(request, 'main/index.html')
+
