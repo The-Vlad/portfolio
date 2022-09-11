0cd99263f065d176c9309200b3ce60082789340c
Эмиль <akhtyamkinemil2004@gmail.com>
2022-09-11 17:18:26 +0400

    Controller Function 31.5
    Controller Function 31.5

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,4 +1,9 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import Http404
+from django.template import TemplateDoesNotExist
+
 
 # Create your views here.
 def about(request):
@@ -7,3 +12,11 @@ def about(request):
 def index(request):
     return render(request, 'main/index.html')
 
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HTTPResponse(template.render(request=request))
+    
