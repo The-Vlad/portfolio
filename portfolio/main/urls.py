abc2ed6a4eb5e465c894c5883b8deaea0ed4d2cf
Эмиль <akhtyamkinemil2004@gmail.com>
2022-09-11 17:27:22 +0400

    Controller Function 31.5
    upd:
    Changes in urls.py

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -1,7 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from . import views as view
+from .views import other_page
+
 
 urlpatterns = [
     path('about/', view.about),
     path('', view.index),
+    path('<str:page>/', view.other_page, name='member')
 ]
